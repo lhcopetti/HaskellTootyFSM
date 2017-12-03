ca63c3b187542a1c3cfc29066b47160d1f64b8ba
lhcopetti <lhcopetti@gmail.com>
2017-12-03 20:28:38 -0200

    INT: First iteration of coming up with a FSM for the Tooty boss of Croc

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,130 @@
 module Main where
 
 import Lib
+import Text.Printf (printf)
+import Control.Monad (foldM)
+import Control.Concurrent (threadDelay)
+
+data TootyState
+    = IdleState
+    | PursuitState
+    | AttackState
+    | RestState
+    | HitState
+    | DyingState
+    | DeadState
+    deriving (Show, Eq)
+
+data TootyEvent
+    = ChaseEvent
+    | CrocIsCloseEvent
+    | TootyIsTiredEvent
+    | TootyIsHitEvent
+    | DieEvent
+    | DeadEvent
+    deriving (Show, Eq)
+
+type FSM s e = s -> e -> IO s
+
+class VarSum r where 
+    variadicSum :: Integer -> r
+
+instance VarSum Integer where 
+    variadicSum = id
+
+instance (Integral a, VarSum r) => VarSum (a -> r) where
+    variadicSum x = variadicSum . (x+) . toInteger
+
+tootyFSM :: FSM TootyState TootyEvent
+tootyFSM IdleState ChaseEvent = do 
+    putStrLn "--- Tooty is chasing after Croc!"
+    return PursuitState
+
+tootyFSM PursuitState CrocIsCloseEvent = do 
+    putStrLn "--- Croc is dangerously close to Tooty!"
+    return AttackState
+
+tootyFSM PursuitState TootyIsTiredEvent = do 
+    putStrLn "--- Tooty grows tired of chasing after Croc!"
+    return RestState
+
+tootyFSM AttackState _ = do 
+    putStrLn "--- Tooty hits Croc!"
+    return RestState
+
+tootyFSM RestState TootyIsHitEvent = do 
+    return DyingState
+
+tootyFSM DyingState _ = return DeadState
+
+
+runFSM :: Foldable f => FSM s e -> s -> f e -> IO s
+runFSM = foldM
+
+loggingFSM :: (Show s, Show e) => FSM s e -> FSM s e
+loggingFSM fsm s e = do 
+    s' <- fsm s e
+    printf "%s × %s → %s\n" (show s) (show e) (show s')
+    return s'
+
+runTootyState :: TootyState -> IO ()
+runTootyState IdleState = putStrLn "Tooty is doing nothing!"
+runTootyState PursuitState = putStrLn "Tooty is running after Croc!"
+runTootyState AttackState = putStrLn "Tooty will hit Croc!"
+runTootyState RestState = do 
+    putStrLn "Tooty is resting!"
+    threadDelay (2 * 10^6)
+runTootyState DyingState = putStrLn "Tooty has been defeated!"
+
+
+getNextEventForState :: TootyState -> IO TootyEvent
+getNextEventForState IdleState = return ChaseEvent
+
+getNextEventForState DyingState = return DeadEvent
+
+getNextEventForState PursuitState = do 
+    putStrLn "Do you wish to get close to Tooty? (Y/n)"
+    result <- getChar
+    if result == 'Y' then 
+        return CrocIsCloseEvent
+    else
+        return TootyIsTiredEvent
+
+getNextEventForState RestState = do
+    putStrLn "Do you wish to hit Tooty? (Y/n)"
+    result <- getChar
+    if result == 'Y' then
+        return TootyIsHitEvent
+    else 
+        return ChaseEvent
+
+getNextEventForState _ = return DieEvent 
+
+runTootyFSM :: TootyState -> IO TootyState
+runTootyFSM s = do
+    runTootyState s
+    event <- getNextEventForState s
+    s' <- tootyFSM s event
+    return s'
+
 
 main :: IO ()
 main = do
     putStrLn "The beginning!"
-    someFunc
+    let initialState = IdleState
+    while (/= DeadState) runTootyFSM initialState
     putStrLn "The end!"
+
+getCharAction :: IO Char
+getCharAction = do
+    x <- getChar
+    putStrLn $ "The value read is: " ++ [x]
+    return x
+
+
+while :: (a -> Bool) -> (a -> IO a) -> a -> IO a
+while predicate action value = do
+    newValue <- action value
+    if predicate newValue then 
+        while predicate action newValue
+    else return newValue
