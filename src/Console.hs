7f08532e95a5aceece5d21aa077547ee91b3a566
lhcopetti <lhcopetti@gmail.com>
2017-12-05 23:21:10 -0200

    INT: Add skeleton implementation of a rotating waiting delay

--- src/Console.hs
+++ src/Console.hs
@@ -1,9 +1,33 @@
-module Console 
-    ( prompt
-    ) where
+module Console  where
+
+import Control.Concurrent (threadDelay)
+import ControlFlow (whileCounter)
 
 prompt :: String -> IO Bool
 prompt xs = do
     putStrLn $ xs ++ " (Y/n)?"
     valueRead <- getLine
     return $ valueRead == "Y"
+
+
+getCharAction :: Char -> IO Char
+getCharAction x = getChar
+
+printLoadingAction :: String -> IO String
+printLoadingAction x = do
+    putStr $ "\r" ++ [head x]
+    threadDelay 500000
+    return $ drop 1 x
+
+printLoadingAction' :: String -> String -> IO String
+printLoadingAction' prompt x = do
+    putStr $ "\r" ++ message
+    threadDelay 500000
+    return $ drop 1 x
+        where
+            message = prompt ++ "... " ++ [head x]
+
+delayWithPrompt :: String -> Int -> IO String
+delayWithPrompt msg count = do 
+    whileCounter count (printLoadingAction' msg) "\\|/-"
+    return "123"
--- src/ControlFlow.hs
+++ src/ControlFlow.hs
@@ -1,5 +1,6 @@
 module ControlFlow 
     ( while
+    , whileCounter
     ) where
 
 while :: (a -> Bool) -> (a -> IO a) -> a -> IO a
@@ -8,3 +9,11 @@ while predicate action value = do
     if predicate newValue then 
         while predicate action newValue
     else return newValue
+
+
+whileCounter :: Int -> (String -> IO String) -> String -> IO String
+whileCounter counter action value
+    | counter > 0 = do
+        newValue <- action value
+        whileCounter (counter - 1) action newValue
+    | otherwise = return $ take 1 value
