258c16094263f5de9c0a24fa85b7d9b3f2ef597d
lhcopetti <lhcopetti@gmail.com>
2017-12-10 19:19:54 -0200

    FIX: Interactive loading prompt was held back in stdout buffer

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,12 @@ module Main where
 import Lib
 import TootyFSM
 import ControlFlow (while)
+import System.IO (hSetBuffering, stdout, BufferMode(..))
 
 
 main :: IO ()
 main = do
+    hSetBuffering stdout NoBuffering
     putStrLn "The beginning!"
     while (/= endFSM) runTootyFSM startFSM
     putStrLn "The end!"
